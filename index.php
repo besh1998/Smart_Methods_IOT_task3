bb4800aab81de18cf144544e771c3d6d50f18575
besh1998 <73873893+besh1998@users.noreply.github.com>
2022-08-15 04:40:26 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<create by github besh1998 >
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>sensor values</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+<style>
+body {
+  background-image: url('sensor.png');
+}
+</style>
+</head>
+<body>
+<form action= "viewValues.php" method="get">
+<form action="" method="post">
+<center>
+<input type="number" name="value" id="name" required="required" placeholder="Please Enter number for the sensor"/><br /><br />
+<input type="submit" value=" Submit " name="submit"/><br />
+</center>
+</form>
+</form>
+</body>
+</html>
